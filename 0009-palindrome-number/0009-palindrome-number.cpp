fcbd502ba7c2a01edba21dc0ae0b7fa6c43ef1f6
Rahul Singh <78529763+RahulSingh-64bit@users.noreply.github.com>
2025-09-08 19:39:23 +0530

    Time: 13 ms (5.42%), Space: 8.5 MB (91.91%), Difficulty:  Easy

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
             return false;
         }
-        
+
         int rev = 0;
-        int num = x;
-        
-        while(num != 0) {
-            rev = rev * 10 + num % 10;
-            num /= 10;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
         }
-        
-        return rev == x;
+
+        return x == rev || x == rev / 10;
     }
 };
